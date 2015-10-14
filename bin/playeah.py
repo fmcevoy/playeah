906a474abb5edac89771ea14e7ca88834e13bf94
Fintan McEvoy <fintan.mcevoy@gmail.com>
2015-10-14 01:11:59 +0000

    simple server

--- bin/playeah.py
+++ bin/playeah.py
@@ -1,2 +1,9 @@
-for i in range(1,100000):
-    print("build test")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
